eb2f533b217f48e37e356e08663854edecd68d79
Mihir Kapur <mihirkapur96@gmail.com>
2021-02-25 16:47:45 -0800

    First Release

---
+++ js/ex3.js
@@ -0,0 +1,4 @@
+/* Homework 6
+Exercise 3 JavaScript code
+*/
+console.log("Mihir's output from Homework 6 Exercise 3");
---
+++ js/ex5.js
@@ -0,0 +1,4 @@
+/* Homework 6
+Exercise 5 JavaScript code
+*/
+console.log("Mihir's output from Homework 6 Exercise 5");
