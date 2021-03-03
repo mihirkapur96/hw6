d0604b0412abe95969cf067f42adb3d5cf47de69
Mihir Kapur <mihirkapur96@gmail.com>
2021-03-02 17:38:35 -0800

    changes

--- js/ex1.js
+++ js/ex1.js
@@ -1,9 +1,8 @@
 /* Homework 6
 Exercise 1 JavaScript code
 */
-/*console.log("Mihir's output from Homework 6 Exercise 1");
+console.log("Mihir's output from Homework 6 Exercise 1");
 const bodyNodes = document.body.childNodes;
 bodyNodes.forEach(node => {
     console.log(node);
-}); */
-console.log(document.body.childNodes[1]);
+});
