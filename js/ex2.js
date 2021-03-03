9f487c290e1b7aea8b6dafe2a3d527bca0be5102
Mihir Kapur <mihirkapur96@gmail.com>
2021-03-02 22:50:06 -0800

    Final Release

--- js/ex2.js
+++ js/ex2.js
@@ -2,14 +2,9 @@
 Exercise 2 JavaScript code
 */
 console.log("Mihir's output from Homework 6 Exercise 2");
-const displayText400= document.getElementById("spring").textContent;
-//displayText400.forEach(element =>{
-  //  console.log(element.textContent)
-//});
-//displayText400();
-console.log(displayText400);
+const displayText400 = () => {
+  const elements= Array.from(document.getElementsByClassName ("400level"));
+  (elements).forEach(elem => console.log(elem.textContent));
+} 
 
-//const displayText400 = (text)=> {
-  //  console.log(document.getElementById("spring").textContent);
-//}
-//displayText400();
+displayText400();
--- js/ex6.js
+++ js/ex6.js
@@ -13,3 +13,21 @@ const word = [{
     term: "Oxymoron",
     definition: "figure of speech that juxtaposes elements that appear to be contradictory"
 }];
+
+const dlItem = document.createElement("dl");
+
+word.forEach(dl => {
+    const dtItem = document.createElement ("dt");
+    const ddItem = document.createElement ("dd");
+
+    dtItem.textContent = dl.term;
+    ddItem.textContent = dl.definition;
+
+    dtItem.style.fontWeight = "bold";
+
+    dlItem.appendChild(dtItem);
+    dlItem.appendChild(ddItem);
+});
+
+const divItem = document.getElementById("content");
+divItem.appendChild(dlItem);
